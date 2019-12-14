647ddc1b33572ae1b6e32b2572e5c4f3ffa0d710
Fyunka <fyunkafinka@hotmail.com>
2019-12-14 23:54:27 +0500

    feat: add two more test suites: 1) test for the search term uRl; 2)test for an empty search term.

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -14,11 +14,34 @@ describe('Filter function', () => {
 
     expect(filterByTerm(input, 'LINK')).toEqual(output); // New test
   });
-});
 
-// function filterByTerm(inputArr, searchTerm) {
-//   const regex = new RegExp(searchTerm, 'i');
-//   return inputArr.filter(function(arrayElement) {
-//     return arrayElement.url.match(regex);
-//   });
-// }
+  // function filterByTerm(inputArr, searchTerm) {
+  //   const regex = new RegExp(searchTerm, 'i');
+  //   return inputArr.filter(function(arrayElement) {
+  //     return arrayElement.url.match(regex);
+  //   });
+  // }
+
+  test('it should be filter by a search term (uRl', () => {
+    const input = [
+      { id: 1, url: 'https://www.url1.dev' },
+      { id: 2, url: 'https://www.url2.dev' },
+      { id: 3, url: 'https://www.link3.dev' }
+    ];
+
+    const output = [
+      { id: 1, url: 'https://www.url1.dev' },
+      { id: 2, url: 'https://www.url2.dev' }
+    ];
+
+    expect(filterByTerm(input, 'uRl')).toEqual(output);
+  });
+
+  test('it should throw when searchTerm is empty string', () => {
+    // solution ex 2
+    const input = [];
+    expect(() => {
+      filterByTerm(input, '');
+    }).toThrowError(Error('searchTerm cannot be empty'));
+  });
+});
